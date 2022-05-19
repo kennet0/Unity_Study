e086feacbf7b3ae0e6af4eeeecfc08cdd5dd8380
HanJang <52649555+kennet0@users.noreply.github.com>
2022-05-20 01:46:03 +0900

    Start

---
+++ CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                Console.WriteLine("직업을 선택하세요!");
+                Console.WriteLine("[1] 기사");
+                Console.WriteLine("[2] 궁수");
+                Console.WriteLine("[3] 법사");
+
+            }
+
+
+
+        }
+    }
+}
