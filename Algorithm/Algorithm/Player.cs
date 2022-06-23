62434fcb6c1c3bee9ccfd76bd714b51ad1e8d338
HanJang <kennet2240@gmail.com>
2022-06-23 18:54:38 +0900

    BFS Algorithm

--- Algorithm/Algorithm/Player.cs
+++ Algorithm/Algorithm/Player.cs
@@ -48,10 +48,12 @@ namespace Algorithm
             int[] deltaX = new int[] { 0, -1, 0, 1 };
 
             bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
 
             Queue<Pos> q = new Queue<Pos>();
             q.Enqueue(new Pos(PosY, PosX));
             found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX);
 
             while(q.Count > 0)
             {
@@ -63,54 +65,76 @@ namespace Algorithm
                 {
                     int nextY = nowY + deltaY[i];
                     int nextX = nowX + deltaX[i];
+
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue; 
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+                    if (found[nextY, nextX])
+                        continue;
+
+                    q.Enqueue(new Pos(nextY, nextX));
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
                 }
             }
 
-        }
-
-        void RightHand()
-        {
-            //현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다.
-            int[] frontY = new int[] { -1, 0, 1, 0 };
-            int[] frontX = new int[] { 0, -1, 0, 1 };
-            int[] rightY = new int[] { 0, -1, 0, 1 };
-            int[] rightX = new int[] { 1, 0, -1, 0 };
-
-            _points.Add(new Pos(PosY, PosX));
-
-            // 목적지 도착하기 전에는 계속 실/
-            while (PosY != _board.DestY || PosX != _board.DestX)
+            int y = _board.DestY;
+            int x = _board.DestX;
+            while(parent[y, x].Y != y || parent[y, x].X != x)
             {
-                // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
-                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
-                {
-                    // 오른쪽 방향으로 90도 회전
-                    _dir = (_dir - 1 + 4) % 4;
-
-                    // 앞으로 한 보 전진
-                    PosY = PosY + frontY[_dir];
-                    PosX = PosX + frontX[_dir];
-                    _points.Add(new Pos(PosY, PosX));
-
-                }
-                // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
-                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
-                {
-                    // 앞으로 한 보 전진
-                    PosY = PosY + frontY[_dir];
-                    PosX = PosX + frontX[_dir];
-                    _points.Add(new Pos(PosY, PosX));
-
-                }
-                else
-                {
-                    // 왼쪽 방향으로 90도 회전
-                    _dir = (_dir + 1 + 4) % 4;
-                }
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
             }
+            _points.Add(new Pos(y, x));
+            _points.Reverse();
         }
 
-        const int MOVE_TICK = 10;
+        //void RightHand()
+        //{
+        //    //현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다.
+        //    int[] frontY = new int[] { -1, 0, 1, 0 };
+        //    int[] frontX = new int[] { 0, -1, 0, 1 };
+        //    int[] rightY = new int[] { 0, -1, 0, 1 };
+        //    int[] rightX = new int[] { 1, 0, -1, 0 };
+
+        //    _points.Add(new Pos(PosY, PosX));
+
+        //    // 목적지 도착하기 전에는 계속 실행 
+        //    while (PosY != _board.DestY || PosX != _board.DestX)
+        //    {
+        //        // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
+        //        if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+        //        {
+        //            // 오른쪽 방향으로 90도 회전
+        //            _dir = (_dir - 1 + 4) % 4;
+
+        //            // 앞으로 한 보 전진
+        //            PosY = PosY + frontY[_dir];
+        //            PosX = PosX + frontX[_dir];
+        //            _points.Add(new Pos(PosY, PosX));
+
+        //        }
+        //        // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
+        //        else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+        //        {
+        //            // 앞으로 한 보 전진
+        //            PosY = PosY + frontY[_dir];
+        //            PosX = PosX + frontX[_dir];
+        //            _points.Add(new Pos(PosY, PosX));
+
+        //        }
+        //        else
+        //        {
+        //            // 왼쪽 방향으로 90도 회전
+        //            _dir = (_dir + 1 + 4) % 4;
+        //        }
+        //    }
+        //}
+
+        const int MOVE_TICK = 100;
         int _sumTick = 0;
         int _lastIndex = 0;
 
