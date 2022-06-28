91e9b62dc698e7a862393775bcf4b6cade5b6a9a
HanJang <kennet2240@gmail.com>
2022-06-29 02:20:31 +0900

    Dijikstra

--- Excercise/Excercise/Program.cs
+++ Excercise/Excercise/Program.cs
@@ -6,7 +6,7 @@ class Graph
     int[,] adj = new int[6, 6]
     {
             {-1, 15, -1, 35, -1, -1 },
-            {15, 0-1 05, 10, -1, -1 },
+            {15, -1, 05, 10, -1, -1 },
             {-1, 05, -1, -1, -1, -1 },
             {35, 10, -1, -1, 05, -1 },
             {-1, -1, -1, 05, -1, 05 },
@@ -69,9 +69,11 @@ class Graph
     {
         bool[] visited = new bool[6];
         int[] distance = new int[6];
+        int[] parent = new int[6];
         Array.Fill(distance, Int32.MaxValue);
 
         distance[start] = 0;
+        parent[start] = start;
 
         // 가장 유력한 후보의 거리와 번호를 저장한다.
         int closest = Int32.MaxValue;
@@ -86,8 +88,41 @@ class Graph
                 if (visited[i])
                     continue;
                 //아직 발견(예약)된 적이 없거나, 기존 후보보다 멀리 있으면 스킵
-                if (distnace[i] == )
-            }    
+                if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                    continue;
+
+                //여태껏 발견한 가장 좋은 후보로 정보갱신
+                closest = distance[i];
+                now = i;
+            }
+
+            // 다음후보가 하나도 없으므로 종료
+            if (now == -1)
+                break;
+
+            // 제일 좋은 후보를 찾았으니 방문한다.
+            visited[now] = true;
+
+            //방문한 정점과 인접한 정점들을 조사해서 상황에따라 발견한 최단거리를 갱신한다.
+            for(int next = 0; next <6; next++)
+            {
+                //연결되지 않은 정점 스킵
+                if (adj[now, next] == -1)
+                    continue;
+                //이 방문한 정점은 스킵
+                if (visited[next])
+                    continue;
+                //새로 조사된 정점의 최단거리를 계산한다.
+                int nextDist = distance[now] + adj[now, next];
+                //만약에 기존에 발견한 최단거리가 새로 조사된 최단거리보다 크면, 정보를 갱신
+                if(nextDist < distance[next])
+                {
+                    distance[next] = nextDist;
+                    parent[next] = now;
+                }
+                
+            }
+
         }
 
     }
@@ -136,7 +171,8 @@ class Program
     {
         Graph graph = new Graph();
         //graph.SearchAll();
-        graph.BFS(0);
+        //graph.BFS(0);
+        graph.Dijikstra(0);
 
     }
 }
