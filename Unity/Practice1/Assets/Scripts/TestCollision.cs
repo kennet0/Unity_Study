c3d37cf1fe340d7ad622cf829458c4502a93d1ff
HanJang <kennet2240@gmail.com>
2022-07-13 02:58:48 +0900

    RayCasting, Camera

--- Unity/Practice1/Assets/Scripts/TestCollision.cs
+++ Unity/Practice1/Assets/Scripts/TestCollision.cs
@@ -14,19 +14,36 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f , Color.red, 1.0f);
 
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+            //int mask = (1 << 8) | (1 << 9);
 
-        RaycastHit[] hits; ;
-        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
-
-        foreach (RaycastHit hit in hits)
-        { 
-
-            Debug.Log($"RayCast! {hit.collider.gameObject.name}!");
-        }
+            RaycastHit hit;
             
+            if (Physics.Raycast(ray, out hit, 100.0f, mask))
+            {
+                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+            }
+        }
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        //    Vector3 dir = mousePos - Camera.main.transform.position;
+        //    dir = dir.normalized;
+
+        //    Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
+
+        //    RaycastHit hit;
+        //    if (Physics.Raycast(Camera.main.transform.position, dir * 100.0f, out hit, 100.0f))
+        //    {
+        //        Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+        //    }
+        //}
     }
 }
