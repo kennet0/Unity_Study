504ef975a17cb64c170fd74cd2f3fcd1dda1d12a
HanJang <kennet2240@gmail.com>
2022-07-15 16:55:35 +0900

    Animation

--- Unity/Practice1/Assets/Scripts/Controllers/PlayerController.cs
+++ Unity/Practice1/Assets/Scripts/Controllers/PlayerController.cs
@@ -17,11 +17,23 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
     }
- 
+
+    float wait_run_ratio = 0 ;
+    public enum PlayerState
+    {
+        Die,
+        Moving,
+        Idle,
+  
+    }
+
+    PlayerState _state = PlayerState.Idle;
+
     void Update()
     {
         if(_moveToDest)
         {
+            
             Vector3 dir = _destPos - transform.position;
             if(dir.magnitude < 0.0001f)
             {
@@ -29,12 +41,28 @@ public class PlayerController : MonoBehaviour
             }
             else
             {
+                
                 float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);            
                 transform.position += dir.normalized * moveDist;
 
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
                 
             }
+        }
+
+        if(_moveToDest)
+        {
+            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
+            Animator anim = GetComponent<Animator>();
+            anim.SetFloat("wait_run_ratio", wait_run_ratio);
+            anim.Play("WAIT_RUN");
+        }
+        else
+        {
+            wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
+            Animator anim = GetComponent<Animator>();
+            anim.SetFloat("wait_run_ratio", wait_run_ratio);
+            anim.Play("WAIT_RUN");
         }
     }
 
@@ -70,9 +98,7 @@ public class PlayerController : MonoBehaviour
 
     void OnMouseClicked(Define.MouseEvent evt)
     {
-        if (evt != Define.MouseEvent.Click)
-            return;
-
+     
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
